e094616585607a1038a6c6b26b07d6253f704a10
Dimas Fajar <153111942+fajardison@users.noreply.github.com>
2025-07-19 17:18:12 +0700

    Update supportedPolicies.js

--- constants/supportedPolicies.js
+++ constants/supportedPolicies.js
@@ -1,5 +1,8 @@
 /**
- * SUPPORTED_POLICIES lists the available cache eviction strategies.
+ * SUPPORTED_POLICIES defines all available cache eviction strategies.
+ * 
+ * The keys represent policy identifiers, and the values are their string names.
+ * This object is immutable and used to validate or reference supported policies.
  */
 export const SUPPORTED_POLICIES = Object.freeze({
   FIFO: 'FIFO',
